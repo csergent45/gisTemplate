a1efef7185646d1b2c5057d5b54d08e3ac65a2e0
Chris Sergent <dsergent@decaturil.gov>
2014-12-31 14:20:04 -0600

    Updated app with  new map coordinates and documentation of CSS, JavaScript, HTML and a readme file for instructions and tips.

--- js/reactor.js
+++ js/reactor.js
@@ -1,11 +1,37 @@
 ﻿
+////////// AUTHOR & APPLICATION INFORMATION ////////////////////////////////////////
+//
+//   Author: Chris Sergent
+//   Date:   December 31, 2014
+//   Application: GIS Template
+//   Page: simpleMap.html
+//
+////////////////////////////////////////////////////////////////////////////////////
 
 
+// Get references to modules to be used
+require(["esri/map",
+         "esri/layers/ArcGISDynamicMapServiceLayer",
+         "dojo/domReady!"], 
+         // Set variables to be used with references (write variables and references in the same order and be careful of typos on your references)
+         function (Map, ArcGISDynamicMapServiceLayer) {
 
-require(["esri/map"], function (Map) {
-    var map = new Map("mapDiv", {
-        basemap: 'streets',
-        center:[-118.2095, 34.0866],
-        zoom:10
+    //-----------------------------------------------------------
+    // Map Services Begin
+    //-----------------------------------------------------------
+
+    // Esri Imagery
+    var map = new Map("mapSection", {
+        basemap: 'satellite',
+        center:[-88.951, 39.861],
+        zoom:12
     });
+
+    // Operation Map Service
+    var operationalLayer = new ArcGISDynamicMapServiceLayer("http://maps.decaturil.gov/arcgis/rest/services/Public/InternetVector/MapServer", { "opacity": 0.5 });
+    map.addLayer(operationalLayer);
+
+    //-----------------------------------------------------------
+    // Map Services End
+    //-----------------------------------------------------------
 });
---
+++ readme.txt
@@ -0,0 +1,6 @@
+﻿Author: Chris Sergent
+Date: December 31, 2014
+Description:
+This is a sample application with multiple web pages as I will build upon this application and integrate Esri samples. Below I will add some notes that are helpful for my development as well as links that may be helpful tools.
+
+Verify HTML has been coded correctly: http://validator.w3.org/
