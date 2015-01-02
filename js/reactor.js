90afe418e23050de4ef524bed7039966af6ddb94
Chris Sergent <dsergent@decaturil.gov>
2015-01-02 16:31:18 -0600

    Add home button to simple map.

--- js/reactor.js
+++ js/reactor.js
@@ -11,27 +11,36 @@
 
 // Get references to modules to be used
 require(["esri/map",
+         "esri/dijit/HomeButton",
          "esri/layers/ArcGISDynamicMapServiceLayer",
-         "dojo/domReady!"], 
-         // Set variables to be used with references (write variables and references in the same order and be careful of typos on your references)
-         function (Map, ArcGISDynamicMapServiceLayer) {
+         "dojo/domReady!"],
+// Set variables to be used with references (write variables and references in the same order and be careful of typos on your references)
+         function (Map, HomeButton, ArcGISDynamicMapServiceLayer) {
 
-    //-----------------------------------------------------------
-    // Map Services Begin
-    //-----------------------------------------------------------
+             //-----------------------------------------------------------
+             // Map Services Begin
+             //-----------------------------------------------------------
 
-    // Esri Imagery
-    var map = new Map("mapSection", {
-        basemap: 'satellite',
-        center:[-88.951, 39.861],
-        zoom:12
-    });
+             // Esri Imagery
+             var map = new Map("mapSection", {
+                 basemap: 'satellite',
+                 center: [-88.951, 39.861],
+                 zoom: 12
+             });
 
-    // Operation Map Service
-    var operationalLayer = new ArcGISDynamicMapServiceLayer("http://maps.decaturil.gov/arcgis/rest/services/Public/InternetVector/MapServer", { "opacity": 0.5 });
-    map.addLayer(operationalLayer);
+             // Operational Map Service
+             var operationalLayer = new ArcGISDynamicMapServiceLayer("http://maps.decaturil.gov/arcgis/rest/services/Public/InternetVector/MapServer", { "opacity": 0.5 });
+             map.addLayer(operationalLayer);
 
-    //-----------------------------------------------------------
-    // Map Services End
-    //-----------------------------------------------------------
-});
+             //-----------------------------------------------------------
+             // Map Services End
+             //-----------------------------------------------------------
+
+             // add home button begin
+             var home = new HomeButton({
+                 map: map
+             }, "homeButton");
+             home.startup();
+             // add home button end
+
+         });
